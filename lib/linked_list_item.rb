58919ead11b14ac7e932296a530091305e0a015d
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-13 23:15:07 -0500

    add solutions to tests for comparator

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -7,14 +7,15 @@ class LinkedListItem
     @payload = data
   end
 
+
   def next_item= (data)
     raise ArgumentError if self == data
     #Note the = sign at the end of the method
-    #When the interpreter sees the message 
-    #"next_item" followed by " =", it automatically ignores the space 
+    #When the interpreter sees the message
+    #"next_item" followed by " =", it automatically ignores the space
     #before equal sign and reads the single message "next_item=" - a call to the method whose name is next_item=, which we've defined.
     #The = connotes a setter method
-    #Ruby lets us define setter methods by placing 
+    #Ruby lets us define setter methods by placing
     #an = at the end
     #Ruby is smart enough to know to drop the = when we
     #call the setter method
@@ -26,4 +27,13 @@ class LinkedListItem
   def last?
     @next_item.nil? || false
   end
+
+  def <=> another
+    self.payload.to_s <=> another.payload.to_s
+  end
+
+  def === another
+    self.inspect.eql?(another.inspect) || false
+  end
+
 end
