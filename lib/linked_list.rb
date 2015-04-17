fc1232314d6941a1a07657a1e730fb2c3d0e097e
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-17 16:05:51 -0500

    add solution to tests 9-10: last item and takes seed arguments

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,27 +3,54 @@ require_relative 'linked_list_item'
 class LinkedList
   attr_reader :size
   attr_reader :last
-  def initialize
+  def initialize(*optional_arg)
     @size = 0
+    if optional_arg[0]
+      optional_arg.each { |x| push(x) }
+    end
+    #@args = optional_arg
+  end
+
+  def [](x)
+
+  end
+
+  def []=(x, y)
   end
 
   def to_s
-    count = 0
-    if @first_item.nil?
-      "| |"
-    else
-      while count < @size
-        arr = []
-        arr << @last_item.payload
-        count += 1
-      end
-        first_part = "| "
-        words = arr.join(" ")
-        last_part = " |"
-        puts first_part + words + last_part
-        first_part + words + last_part
+    str = "| "
+    current_item = @first_item
+    until current_item.nil?
+      str << current_item.payload
+      punctuation = current_item.last? ? " " : ", "
+      str << punctuation
+      current_item = current_item.next_item #this line resets the current item so the loop goes out and gets the next item
     end
+    str << "|"
+    str
   end
+
+  #version of to_s method given in class
+=begin
+  def to_s
+    str = "| "
+    #loop goes here
+    size.times do |i|
+      str << get(i) #get implicitly calls self
+      if i == (size - 1)
+        str << " "
+      else
+        str << ", "
+      end
+    end
+    unless @first_item.nil?
+      str << @first_item.payload + " "
+    end
+    str << "|"
+    str
+  end
+=end
 
 
   def get(nth_item)
